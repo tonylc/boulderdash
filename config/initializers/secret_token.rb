e283116655fb58397f818c541cea59c47e44f474
tony <tony@songkick.com>
2011-12-30 20:05:53 +0000

    add everything

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boulderdash::Application.config.secret_token = '358190450394cf490920a353c183fd5850c3b97829be86210fe79c9855b7dc37a56e29499e702217f3093656eab8df59e5cbe8397a5fdec136ad20e7c3b7841a'
